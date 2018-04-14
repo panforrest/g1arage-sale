47c64195979f5d58c82fa6cdc70e422db92d2b42
panforrest <guoqianp@gmail.com>
2018-04-14 10:51:34 -0400

    add new items via reducer to Search and Results components with static geoCords but by inputting name price

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,36 +9,43 @@ import { TurboClient } from '../utils'
 
 export default {
 
-	fetchUsers: (params) => {
-		return dispatch => {
-			return dispatch(TurboClient.getRequest('user', params, constants.USERS_RECEIVED))
-		}
-	},
-
-	addUser: (params) => {
-		return dispatch => {
-			return dispatch(TurboClient.postRequest('user', params, constants.USER_CREATED))
-		}
-	},
-
-	// Unlike addUser, register() also maintains a session for login state. After calling 
-	// TurboClient.createUser(), the new user is logged in as well:
-	register: (params) => {
-		return dispatch => {
-			return dispatch(TurboClient.createUser(params, constants.USER_CREATED))
-		}
-	},
-
-	loginUser: (credentials) => {
-		return dispatch => {
-			return dispatch(TurboClient.login(credentials, constants.CURRENT_USER_RECEIVED))
-		}
-	},
-
-	currentUser: () => {
-		return dispatch => {
-			return dispatch(TurboClient.currentUser(constants.CURRENT_USER_RECEIVED))
+	addItem: (item) => {
+		return {
+			type: 'ITEM_ADDED',
+			data: item
 		}
 	}
+
+	// fetchUsers: (params) => {
+	// 	return dispatch => {
+	// 		return dispatch(TurboClient.getRequest('user', params, constants.USERS_RECEIVED))
+	// 	}
+	// },
+
+	// addUser: (params) => {
+	// 	return dispatch => {
+	// 		return dispatch(TurboClient.postRequest('user', params, constants.USER_CREATED))
+	// 	}
+	// },
+
+	// // Unlike addUser, register() also maintains a session for login state. After calling 
+	// // TurboClient.createUser(), the new user is logged in as well:
+	// register: (params) => {
+	// 	return dispatch => {
+	// 		return dispatch(TurboClient.createUser(params, constants.USER_CREATED))
+	// 	}
+	// },
+
+	// loginUser: (credentials) => {
+	// 	return dispatch => {
+	// 		return dispatch(TurboClient.login(credentials, constants.CURRENT_USER_RECEIVED))
+	// 	}
+	// },
+
+	// currentUser: () => {
+	// 	return dispatch => {
+	// 		return dispatch(TurboClient.currentUser(constants.CURRENT_USER_RECEIVED))
+	// 	}
+	// }
 	
 }
--- src/reducers/itemReducer.js
+++ src/reducers/itemReducer.js
@@ -2,7 +2,7 @@ var initialState = {
     all: [
             {id:1, key:'1', price:10, defaultAnimation:2, label:'Nike Jordans', position:{lat:40.7224017, lng:-73.9896719}},
             {id:2, key:'2', price:20, defaultAnimation:2, label:'Sofa', position:{lat:40.7124017, lng:-73.9896719}},
-            {id:3, key:'3', price:30, defaultAnimation:2, label:'Sofa', position:{lat:40.7024017, lng:-73.9896719}}
+            {id:3, key:'3', price:30, defaultAnimation:2, label:'TV', position:{lat:40.7024017, lng:-73.9896719}}
     ]	
 }
 
@@ -10,6 +10,14 @@ export default (state=initialState, action) => {
 	let updated = Object.assign({}, state)
 
 	switch(action.type){
+
+        case 'ITEM_ADDED':
+            console.log('ITEM_ADDED: '+JSON.stringify(action.data))
+            let all = Object.assign([], updated.all)
+            all.push(action.data)
+            updated['all'] = all
+            return updated
+
 		default: 
 		    return state
 	}
